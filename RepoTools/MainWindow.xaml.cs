eb797b1041860f905dca28d7f84bc9b9a5645050
SclRlLiX <georg.m985@gmail.com>
2024-04-04 16:24:32 +0200

    Added Logic to get Package Names and Versions

--- RepoTools/MainWindow.xaml.cs
+++ RepoTools/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media;
 
 namespace RepoTools
 {
@@ -7,22 +10,168 @@ namespace RepoTools
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string option1 = "Neue Paketversion";
+        private readonly string option2 = "Neues Paket";
+        private readonly string option3 = "Bestehende Paketversion anpassen";
+
         public MainWindow()
         {
             InitializeComponent();
 
+            //Set Default values
+            DefaultValues();
+        }
+
+        private void DefaultValues()
+        {
             // ###################################################
             // ############### FORM DEFAULT VALUES ############### 
             // ###################################################
 
 
-            //Combo Box cbChooseOption
-            cbChooseOption.Items.Add("Neue Paketversion");
-            cbChooseOption.Items.Add("Neues Paket");
-            cbChooseOption.Items.Add("Bestehende Paketversion anpassen");
+            //Fill ComboBox cbChooseOption
+            string[] options = [option1, option2, option3];
+            cbChooseOption.ItemsSource = options;
 
-            // Combo Box 
+            // Disable all other fields
             cbChooseFolder.IsEnabled = false;
+            cbChoosePackage.IsEnabled = false;
+            cbChoosePackageVersion.IsEnabled = false;
+            cbxDcsEntw.IsEnabled = false;
+            cbxDcsTest.IsEnabled = false;
+            cbxDcsProd.IsEnabled = false;
+            cbxStvmv.IsEnabled = false;
+            cbxSccm.IsEnabled = false;
+            tbxOrderId.IsEnabled = false;
+            tbxRemark.IsEnabled = false;
+            tbxSoftwareVersion.IsEnabled = false;
+            cbxAddToMail.IsEnabled = false;
+            btnCancel.IsEnabled = false;
+            btnSubmit.IsEnabled = false;
+
+            //Reset ChooseFolder field
+            cbChooseFolder.ItemsSource = null;
+
+            //Reset ChoosePackage field
+            lblChoosePackage.Content = "Paket wählen";
+            lblChoosePackage.Foreground = Brushes.Black;
+            cbChoosePackage.ItemsSource = null;
+
+            //Reset ChoosePackageVersion field
+            lblChoosePackageVersion.Content = "Paketversion wählen:";
+            lblChoosePackageVersion.Foreground = Brushes.Black;
+            cbChoosePackageVersion.ItemsSource = null;
+        }
+
+
+        //cbChooseOption 
+        private void cbChooseOption_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            //Load Default Values
+            DefaultValues();
+
+            //Get Packages from SVN-Archiv Folder 
+            ArrayList packageFoldersWithoutSvnFolder = SvnArchivFolderPackages.GetPackagesWithoutSvnFolder();
+            ArrayList packageFoldersWithSvnFolder = SvnArchivFolderPackages.GetPackagesWithSvnFolder();
+
+            //Display Packages that already exist in SVN Repo
+            if ((string)cbChooseOption.SelectedItem == option1 || (string)cbChooseOption.SelectedItem == option3)
+            {
+                // Enable - Disable fields
+                cbChooseFolder.IsEnabled = false;
+                cbChoosePackage.IsEnabled = true;
+
+                //Clear cbChooseFolder
+                cbChooseFolder.ItemsSource = null;
+
+                //Display Packages from SVN Archiv Folder (with .svn Folder)
+                if (packageFoldersWithSvnFolder.Count > 0)
+                {
+                    cbChoosePackage.ItemsSource = packageFoldersWithSvnFolder;
+
+                    lblChoosePackage.Content = "Paket wählen";
+                    lblChoosePackage.Foreground = Brushes.Black;
+                }
+                else
+                {
+                    lblChoosePackage.Content = "Kein(e) Paket(e) vorhanden! ";
+                    lblChoosePackage.Foreground = Brushes.Red;
+                }
+                
+            }
+
+            //Display Packages that do not exist in SVN Repo
+            if ((string)cbChooseOption.SelectedItem == option2)
+            {
+                // Enable next fields 
+                cbChooseFolder.IsEnabled = true;
+                cbChoosePackage.IsEnabled = true;
+
+                //Display all Repo Folders
+                cbChooseFolder.ItemsSource = RepoFolders.GetRepoFolders();
+
+                //Display Packages from SVN Archiv Folder (without .svn Folder)
+                if (packageFoldersWithoutSvnFolder.Count > 0)
+                {
+                    cbChoosePackage.ItemsSource = packageFoldersWithoutSvnFolder;
+
+                    lblChoosePackage.Content = "Paket wählen";
+                    lblChoosePackage.Foreground = Brushes.Black;
+                }
+                else
+                {
+                    lblChoosePackage.Content = "Kein(e) Paket(e) vorhanden! ";
+                    lblChoosePackage.Foreground = Brushes.Red;
+                }
+            }
+
+
+
+        }
+
+        //cbChoosePackage
+        private void cbChoosePackage_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if(cbChoosePackage.Items.Count > 0) 
+            {
+                //Enable and fill PackageVersion Field
+                cbChoosePackageVersion.IsEnabled = true;
+                ArrayList svnArchivFolderPackageVersions = SvnArchivFolderPackageVersions.GetPackageVersions(cbChoosePackage.SelectedValue.ToString());
+                if(svnArchivFolderPackageVersions.Count > 0) 
+                {
+                    cbChoosePackageVersion.ItemsSource = svnArchivFolderPackageVersions;
+                    lblChoosePackageVersion.Content = "Paketversion wählen:";
+                    lblChoosePackageVersion.Foreground = Brushes.Black;
+                }
+                else
+                {
+                    lblChoosePackageVersion.Content = "Keine Version Verfügbar!";
+                    lblChoosePackageVersion.Foreground = Brushes.Red;
+                }
+                
+            }
+
+        }
+
+        //cbChoosePackageVersion
+        private void cbChoosePackageVersion_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if(cbChoosePackageVersion.Items.Count > 0) 
+            {
+                //Enable all other fields
+                cbxDcsEntw.IsEnabled = true;
+                cbxDcsTest.IsEnabled = true;
+                cbxDcsProd.IsEnabled = true;
+                cbxStvmv.IsEnabled = true;
+                cbxSccm.IsEnabled = true;
+                tbxOrderId.IsEnabled = true;
+                tbxRemark.IsEnabled = true;
+                tbxSoftwareVersion.IsEnabled = true;
+                cbxAddToMail.IsEnabled = true;
+                btnCancel.IsEnabled = true;
+                btnSubmit.IsEnabled = true;
+            }
+
         }
     }
 }
