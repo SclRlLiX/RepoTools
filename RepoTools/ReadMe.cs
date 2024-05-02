fe736d06a254649bf9e5208e79607cc1a72446b4
SclRlLiX <georg.m985@gmail.com>
2024-05-02 16:20:04 +0200

    created class to create a readme file

---
+++ RepoTools/ReadMe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoTools
+{
+    internal class ReadMe
+    {
+        public SvnCheckInObject? SvnCheckInObject {  get; set; }
+
+        public void CreateAndSaveReadme()
+        {
+            string readme = $@"
+# {SvnCheckInObject?.PackageName} {SvnCheckInObject?.PackageVersion}
+            
+## Allgemeine Information
+
+Paketname | Version | Software Version | Archiv
+--- | --- | ---
+**{SvnCheckInObject?.PackageName}** | ``{SvnCheckInObject?.PackageVersion}`` | ``{SvnCheckInObject?.SoftwareVersion}`` | *{SvnCheckInObject?.RepoFolder}*
+    
+";
+
+            File.WriteAllText("C:\\SVN-Archiv\\readme.md", readme);
+
+            Debug.WriteLine(readme);
+        }
+
+    }
+}
